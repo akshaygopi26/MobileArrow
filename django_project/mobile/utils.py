2ddbf6f983d82f754f65096d43929fdd99cd334f
Akshay G <22m0788@iitb.ac.in>
2022-11-23 02:17:07 +0530

    matplotlib brand vs unitssold graph added

--- django_project/mobile/utils.py
+++ django_project/mobile/utils.py
@@ -1,23 +1,30 @@
 import matplotlib.pyplot as plt 
 import base64
 from io import BytesIO
+import csv, sys, re, random, os, time
+import numpy as np
 
 
 def get_graph():
-    buffer = BytesIO
+    buffer = BytesIO()
     plt.savefig(buffer,format='png')
     buffer.seek(0)
     image_png=buffer.getvalue()
-    print(image_png)
     graph=base64.b64encode(image_png)
-    print(graph)
     graph=graph.decode('utf-8')
-    print(graph)
     buffer.close()
     return graph
 
 
 def get_plot(x,y):
-    #put your code here 
+    #put your code here
+    plt.switch_backend('AGG')
+    # plt.figure(figsize=(10,5))
+    plt.title('Number of units sold per brand')
+    plt.bar(x,y, width=0.4, color='red',edgecolor='black',label='Units Sold')
+    plt.xlabel('Brand Name')
+    plt.ylabel('Units Sold(In Thousands)')
+    plt.legend(loc=0)
+    plt.tight_layout()
     graph=get_graph()
     return graph
