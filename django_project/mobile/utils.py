82fe596e49e78aae40f518da5448d04e839cc95d
Akshay G <22m0788@iitb.ac.in>
2022-11-22 19:51:16 +0530

    added utils.py

--- django_project/mobile/utils.py
+++ django_project/mobile/utils.py
@@ -1,2 +1,14 @@
 import matplotlib.pyplot as plt 
 import base64
+from io import BytesIO
+
+
+def get_graph():
+    buffer = BytesIO
+    plt.savefig  
+
+
+def get_plot(x,y):
+    #put your code here 
+    graph=get_graph()
+    return graph
