6a46f577906bab3c02c929dfa637b8a5884ef0d3
Akshay G <22m0788@iitb.ac.in>
2022-11-22 23:03:55 +0530

    utils.py change

--- django_project/mobile/utils.py
+++ django_project/mobile/utils.py
@@ -5,7 +5,16 @@ from io import BytesIO
 
 def get_graph():
     buffer = BytesIO
-    plt.savefig  
+    plt.savefig(buffer,format='png')
+    buffer.seek(0)
+    image_png=buffer.getvalue()
+    print(image_png)
+    graph=base64.b64encode(image_png)
+    print(graph)
+    graph=graph.decode('utf-8')
+    print(graph)
+    buffer.close()
+    return graph
 
 
 def get_plot(x,y):
